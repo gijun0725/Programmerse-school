622d1ef66243bfa32ae9b9b363999190bbde1e64
GiJunLee <119472512+gijun0725@users.noreply.github.com>
2023-05-03 20:42:19 +0900

    Update 3진법 뒤집기.py

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -9,3 +9,4 @@ def solution(n):
     for i in range(len(answer)):
         answers+=int(answer[i])*int(3**i)
     return answers
+#3진법 계산 방법만 안다면 쉽게 가능하다 굳이 핵심이라 하면 3**i를 통해서 i가 증가함에 다라 제곱의 수가 바뀌는것만 캐치한다면 쉬웠을 것이다
