f0756bd2788167034bbebf83f54157930367443c
GiJunLee <119472512+gijun0725@users.noreply.github.com>
2023-05-05 23:25:07 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -0,0 +1,5 @@
+def solution(a, b):
+    days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']  # 1월 1일이 금요일이므로 금요일부터 시작
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # 각 월의 일수를 리스트로 저장
+    days_passed = sum(months[:a-1]) + b - 1  # 입력된 월까지의 일수와 일(day)을 더해서 전체 일 수를 계산
+    return days[days_passed % 7]  # 전체 일 수를 7로 나눈 나머지를 인덱스로 사용하여 요일 반환
