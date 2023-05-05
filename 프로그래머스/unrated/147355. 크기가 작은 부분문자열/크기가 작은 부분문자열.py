c2b975d70128963910303d66f84c326c4a4b61a5
GiJunLee <119472512+gijun0725@users.noreply.github.com>
2023-05-05 19:16:19 +0900

    Delete 프로그래머스/unrated/147355. 크기가 작은 부분문자열 directory

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++
@@ -1,8 +0,0 @@
-def solution(t, p):
-    answer = 0
-    z=len(p)
-    for i in range(len(t)):
-        if (len(t[i:i+z])==z) and (int(t[i:i+z])<=int(p)):
-            answer+=1
-
-    return answer
