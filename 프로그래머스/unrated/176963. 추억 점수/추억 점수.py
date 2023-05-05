ab7117260d957d178ebb2f9f376576908453d707
GiJunLee <119472512+gijun0725@users.noreply.github.com>
2023-05-05 19:52:35 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,11 @@
+def solution(name, yearning, photo):
+    answer = []
+    for i in range(len(photo)):
+        sum=0
+        for j in range(len(photo[i])):
+            if (photo[i][j]) in name:
+                sum+=yearning[name.index(photo[i][j])]
+            else:
+                pass
+        answer.append(sum)
+    return answer
