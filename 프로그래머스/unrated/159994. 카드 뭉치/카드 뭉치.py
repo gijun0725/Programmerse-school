5b5cc289f039ce9f00a3e96b60f085a6c2a54e0c
GiJunLee <119472512+gijun0725@users.noreply.github.com>
2023-05-09 22:44:35 +0900

    Update 카드 뭉치.py

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -11,3 +11,7 @@ def solution(cards1, cards2, goal):
             answer.append(cards2[j])
             j+=1
     return 'Yes' if answer==goal else 'No'
+
+#카드 뭉치의 핵심은 두개의 i,j를 따로 접근하면서 하나씩 증가시키는 방법이다
+#보통 for문을 이용해서 두개를 접근했는데 for문에만 집착하다보니 간단한 방법을 어떻게 구현하는지 까먹었다
+#접근방법은 맞았으나 코드로 구현하는걸 실패했던 문제 다시한번 풀면 맞는다
