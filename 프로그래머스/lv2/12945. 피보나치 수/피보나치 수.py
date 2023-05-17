2661ca6611e6b9e26881ee10421e0cd6d566e037
GiJunLee <119472512+gijun0725@users.noreply.github.com>
2023-05-17 09:48:40 +0900

    Update 피보나치 수.py

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -4,3 +4,4 @@ def solution(n):
     for i in range(2,n+1):
         answer.append((answer[i-2]+answer[i-1])%1234567)
     return answer[-1]
+#피보나치수는 이전값을 가져와서 활용해야 하기 때문에 배열에 하나씩 추가해서 i-2부터 다시 가져오면 값을 계속 이용할 수 있다
