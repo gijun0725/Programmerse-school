1d608ab9ae036ca60f6b4e8c89dbd1ea90a469b6
GiJunLee <119472512+gijun0725@users.noreply.github.com>
2023-05-19 09:00:48 +0900

    Update 덧칠하기.py

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -5,3 +5,5 @@ def solution(n, m, section):
             painted=i+m
             answer+=1
     return answer
+#덧칠하기 핵심 시작 painted=0부터 만약 i(section)이 나왔을때 칠해진 부분과 크거나 같다면 페인트 칠을 해야하니까
+#painted = i(section)부터 m(롤러길이) 만큼 칠하고 painted된 영역을 업데이트 해주고 한번칠했으니 answer를 업데이트 하면된다
